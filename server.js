8ab5ee3029b4a45b10c352a3f87038dfa56e6311
Mishon-pon-pon <terem848@gmail.com>
2021-08-23 13:10:27 +0300

    hotfix port 3000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const serverless = require('serverless-http');
 const app = express();
 
-const Port = 3002;
+const Port = 3000;
 
 // app.use(express.static(__dirname + '/dist'));
 // module.exports.handler = serverless(app);
